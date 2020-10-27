4987ef1b04e2ec74f290743f1babdd8f7abc59fa
Thomas Bonk <thomas@meandmymac.de>
2020-10-27 15:46:56 +0100

    renamed library

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "mi-casa-plugin",
+            name: "MiCasaPlugin",
             targets: ["mi-casa-plugin"]),
     ],
     dependencies: [
