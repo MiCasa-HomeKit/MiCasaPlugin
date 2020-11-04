296eed9e45529ce2d8b8d3b452f0f279c2daee88
Thomas Bonk <thomas@meandmymac.de>
2020-11-04 17:12:18 +0100

    fixed swiftlint issues

--- Sources/MiCasaPlugin/ApiGateway.swift
+++ Sources/MiCasaPlugin/ApiGateway.swift
@@ -1,88 +1,87 @@
 /*
- Copyright 2020 MiCasa Development Team
+  Copyright 2020 MiCasa Development Team
 
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
 
- http://www.apache.org/licenses/LICENSE-2.0
+  http://www.apache.org/licenses/LICENSE-2.0
 
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
- */
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
 
 import Foundation
 import HAP
 
 /**
- API Gateway for MiCasa plugins.
+  API Gateway for MiCasa plugins.
 
- - Author: Thomas Bonk <thomas@meandmymac.de>
- */
+  - Author: Thomas Bonk <thomas@meandmymac.de>
+  */
 public protocol ApiGateway {
+  // MARK: - Bridge API
 
-    // MARK: - Bridge API
-
-    /// All accessories that are currently provided by MiCasa
-    var accessories: [Accessory] { get }
+  /// All accessories that are currently provided by MiCasa
+  var accessories: [Accessory] { get }
 
 
-    // MARK: - Logging
+  // MARK: - Logging
 
-    /**
-     This method can be called to log an info message.
+  /**
+    This method can be called to log an info message.
 
-     - Parameters:
-         - plugin: the plugin that wants to log a message
-         - message: the message to be logged
-     */
-    func info(plugin: MiCasaPlugin, message: String)
+    - Parameters:
+      - plugin: the plugin that wants to log a message
+      - message: the message to be logged
+    */
+  func info(plugin: MiCasaPlugin, message: String)
 
-    /**
-     This method can be called to log a warning message.
+  /**
+    This method can be called to log a warning message.
 
-     - Parameters:
-         - plugin: the plugin that wants to log a message
-         - message: the message to be logged
-     */
-    func warning(plugin: MiCasaPlugin, message: String)
+    - Parameters:
+      - plugin: the plugin that wants to log a message
+      - message: the message to be logged
+    */
+  func warning(plugin: MiCasaPlugin, message: String)
 
-    /**
-     This method can be called to log an error message.
+  /**
+    This method can be called to log an error message.
 
-     - Parameters:
-         - plugin: the plugin that wants to log a message
-         - message: the message to be logged
-     */
-    func error(plugin: MiCasaPlugin, message: String)
+    - Parameters:
+      - plugin: the plugin that wants to log a message
+      - message: the message to be logged
+    */
+  func error(plugin: MiCasaPlugin, message: String)
 
-    /**
-     This method can be called to log a critical error message.
+  /**
+    This method can be called to log a critical error message.
 
-     - Parameters:
-         - plugin: the plugin that wants to log a message
-         - message: to be logged
-     */
-    func critical(plugin: MiCasaPlugin, message: String)
+    - Parameters:
+      - plugin: the plugin that wants to log a message
+      - message: to be logged
+    */
+  func critical(plugin: MiCasaPlugin, message: String)
 
-    /**
-     This method can be called to log a debug message.
+  /**
+    This method can be called to log a debug message.
 
-     - Parameters:
-         - plugin: the plugin that wants to log a message
-         - message: the message to be logged
-     */
-    func debug(plugin: MiCasaPlugin, message: String)
+    - Parameters:
+      - plugin: the plugin that wants to log a message
+      - message: the message to be logged
+    */
+  func debug(plugin: MiCasaPlugin, message: String)
 
-    /**
-     This method can be called to log a trace message.
+  /**
+    This method can be called to log a trace message.
 
-     - Parameters:
-         - plugin: the plugin that wants to log a message
-         - message: the message to be logged
-     */
-    func trace(plugin: MiCasaPlugin, message: String)
+    - Parameters:
+      - plugin: the plugin that wants to log a message
+      - message: the message to be logged
+    */
+  func trace(plugin: MiCasaPlugin, message: String)
 }
--- Sources/MiCasaPlugin/MiCasaPlugin.swift
+++ Sources/MiCasaPlugin/MiCasaPlugin.swift
@@ -1,117 +1,112 @@
 /*
- Copyright 2020 MiCasa Development Team
+  Copyright 2020 MiCasa Development Team
 
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
 
- http://www.apache.org/licenses/LICENSE-2.0
+  http://www.apache.org/licenses/LICENSE-2.0
 
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
- */
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
 
 import Foundation
 import HAP
 
 
 /**
- This is the base class for general MiCasa plugins.
+  This is the base class for general MiCasa plugins.
 
- General MiCasa plugins don't provide any HomeKit accessories.
- They can be used to provide web-based UIs, REST services for
- accessing MiCasa from apps etc.
+  General MiCasa plugins don't provide any HomeKit accessories.
+  They can be used to provide web-based UIs, REST services for
+  accessing MiCasa from apps etc.
 
- - Author: Thomas Bonk <thomas@meandmymac.de>
- */
+  - Author: Thomas Bonk <thomas@meandmymac.de>
+  */
 open class MiCasaPlugin: Hashable {
+  // MARK: - Private Propereties
 
-    // MARK: - Private Propereties
-
-    public private(set) var apiGateway: ApiGateway!
-
-
-    // MARK: - Initialization
-
-    /**
-     Initialize the plugin.
-
-     - Parameters:
-        - apiGateway: The API gateway that provides MiCasa functions
-        - configuration: The configuration for the plugin as encoded JSON
-     */
-    public init?(apiGateway gateway: ApiGateway, configuration: Data) throws {
-        self.apiGateway = gateway
-    }
+  public private(set) var apiGateway: ApiGateway
 
 
-    // MARK: - Hashable
+  // MARK: - Initialization
 
-    public static func == (lhs: MiCasaPlugin, rhs: MiCasaPlugin) -> Bool {
-        fatalError("You have to override the method `==(lhs:rhs:)`.")
-    }
+  /**
+    Initialize the plugin.
 
-    open var hashValue: Int {
-        fatalError("You have to override the property `hashValue`.")
-    }
+    - Parameters:
+      - apiGateway: The API gateway that provides MiCasa functions
+      - configuration: The configuration for the plugin as encoded JSON
+    */
+  public init?(apiGateway gateway: ApiGateway, configuration: Data) throws {
+    self.apiGateway = gateway
+  }
 
-    open func hash(into hasher: inout Hasher) {
-        fatalError("You have to override the method `hash(into:)`.")
-    }
+
+  // MARK: - Hashable
+
+  public static func == (lhs: MiCasaPlugin, rhs: MiCasaPlugin) -> Bool {
+    fatalError("You have to override the method `==(lhs:rhs:)`.")
+  }
+
+  open func hash(into hasher: inout Hasher) {
+    fatalError("You have to override the method `hash(into:)`.")
+  }
 
 
-    // MARK: - API
+  // MARK: - API
 
-    /// Convert the configuration given as JSON to a structured type.
-    ///
-    /// - Parameters:
-    ///   - json: The configuration as JSON data
-    ///
-    /// - Returns: The the configuration converted to a structured type.
-    open func configuration<T: Codable>(_ json: Data) throws -> T {
-        let decoder = JSONDecoder()
+  /// Convert the configuration given as JSON to a structured type.
+  ///
+  /// - Parameters:
+  ///   - json: The configuration as JSON data
+  ///
+  /// - Returns: The the configuration converted to a structured type.
+  open func configuration<T: Codable>(_ json: Data) throws -> T {
+    let decoder = JSONDecoder()
 
-        decoder.keyDecodingStrategy = .convertFromSnakeCase
+    decoder.keyDecodingStrategy = .convertFromSnakeCase
 
-        return try decoder.decode(T.self, from: json)
-    }
+    return try decoder.decode(T.self, from: json)
+  }
 
-    /// This method starts the plugin.
-    open func start() throws {
-        // Empty by design
-    }
+  /// This method starts the plugin.
+  open func start() throws {
+    // Empty by design
+  }
 
-    /// This method stops the plugin.
-    ///
-    /// The method is called before the bridge stops or is about to restart.
-    open func stop() throws {
-        // Empty by design
-    }
+  /// This method stops the plugin.
+  ///
+  /// The method is called before the bridge stops or is about to restart.
+  open func stop() throws {
+    // Empty by design
+  }
 
-    // MARK: - Accessory API
+  // MARK: - Accessory API
 
-    /**
-     This method must be overriden and has to return the accessories that are
-     provided by this plugin.
+  /**
+    This method must be overriden and has to return the accessories that are
+    provided by this plugin.
 
-     - Returns: The accessories that are provided by this plugin.
-     */
-    open func accessories() throws -> [Accessory] {
-        return []
-    }
+    - Returns: The accessories that are provided by this plugin.
+    */
+  open func accessories() throws -> [Accessory] {
+    return []
+  }
 
-    open func identify(accessory: Accessory) throws {
+  open func identify(accessory: Accessory) throws {
+  }
 
-    }
-
-    open func characteristic<T>(
-        _ characteristic: GenericCharacteristic<T>,
-        ofService service: Service,
-        ofAccessory accessory: Accessory,
-        didChangeValue newValue: T?) throws {
-
-    }
+  open func characteristic<T>
+  (
+    _ characteristic: GenericCharacteristic<T>,
+    ofService service: Service,
+    ofAccessory accessory: Accessory,
+    didChangeValue newValue: T?
+  ) throws {
+  }
 }
